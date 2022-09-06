6d27940deef704c488c29ba8dbd8c0e0fdafe8a3
Bloody <bloodyeyesblackblood@gmail.com>
2022-09-06 22:58:44 +0200

    heh

---
+++ .eleventy.js
@@ -0,0 +1,28 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+
+  eleventyConfig.addWatchTarget("src/css/");
+  
+    eleventyConfig.addCollection('posts', function(collectionApi) {
+	return collectionApi.getFilteredByGlob('src/posts/*.md');
+	
+eleventyConfig.addFilter("excerpt", (post) => {
+  const content = post.replace(/(<([^>]+)>)/gi, "");
+  return content.substr(0, content.lastIndexOf(" ", 10)) + "...";
+});
+})
+
+
+  return {
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+  };
+}
+
+
+
+
+
