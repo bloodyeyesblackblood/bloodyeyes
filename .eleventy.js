6bad7ba9691ee48a712ca2884a0061a02fd8d661
bloodyeyesblackblood <108025342+bloodyeyesblackblood@users.noreply.github.com>
2022-09-06 23:30:34 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -7,10 +7,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addCollection('posts', function(collectionApi) {
 	return collectionApi.getFilteredByGlob('src/posts/*.md');
 	
-eleventyConfig.addFilter("excerpt", (post) => {
-  const content = post.replace(/(<([^>]+)>)/gi, "");
-  return content.substr(0, content.lastIndexOf(" ", 10)) + "...";
-});
 })
 
 
